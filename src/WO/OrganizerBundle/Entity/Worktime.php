6887d176d1b9f4dbb1283fc0fe5ec0cb96e8cb85
Tom <Tom.Scheduikat@gmail.com>
2015-05-28 10:03:48 +0200

    hotfix

--- src/WO/OrganizerBundle/Entity/Worktime.php
+++ src/WO/OrganizerBundle/Entity/Worktime.php
@@ -94,7 +94,7 @@ class Worktime
     /**
      * @var string
      *
-     * @ORM\Column(name="info", type="string", length=255)
+     * @ORM\Column(name="info", type="string", length=255, nullable=true)
      */
     private $info;
 
